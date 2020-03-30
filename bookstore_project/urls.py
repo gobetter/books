ac697f2f1a9c308de3097fee83783b37e102455e
Isara Chumsri <gobetter@gmail.com>
2020-03-30 17:23:25 +0700

    ch7

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include  # new
+from django.urls import path, include
 
 urlpatterns = [
     # Django admin
     path('admin/', admin.site.urls),
 
     # User management
-    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
 
     # Local apps
-    path('accounts/', include('users.urls')),
+    # path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
